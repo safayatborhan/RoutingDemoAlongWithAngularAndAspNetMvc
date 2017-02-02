9188a257fb4f70f31c401071271abc8819d20c70
Safayat Zisan <zisan_cse_mbstu@hotmail.com>
2017-02-03 02:30:27 +0600

    First Commit

---
+++ RoutingDemo/Angulars/Controllers.js
@@ -0,0 +1,30 @@
+﻿
+var angualarModule = angular.module("angualarModule", ['ngRoute']);
+
+angualarModule.controller("indexController", function ($scope) {
+    $scope.name = "Zisan";    
+})
+
+angualarModule.config(function ($routeProvider) {
+    $routeProvider.
+    when('/firstPage', {
+        templateUrl: 'RouterDemo/First',
+        controller: 'routeDemoFirstController'
+    }).
+    when('/secondPage', {
+        templateUrl: 'RouterDemo/Second',
+        controller: 'routeDemoSecondController'
+    })
+})
+
+angualarModule.controller("routeDemoFirstController", function ($scope) {
+    $scope.FirstName = "Safayat";
+    $scope.LastName = "Zisan";
+})
+
+angualarModule.controller("routeDemoSecondController",function ($scope) {
+    $scope.MobileNumber = "55";
+    $scope.EmailID = "zisan_cse_mbstu@hotmail.com";
+})
+
+
---
+++ RoutingDemo/Controllers/RouterDemoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingDemo.Controllers
+{
+    public class RouterDemoController : Controller
+    {
+        // GET: RouterDemo
+        public ActionResult First()
+        {
+            return View();
+        }
+
+        public ActionResult Second()
+        {
+            return View();
+        }
+    }
+}
